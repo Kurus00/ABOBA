eebba9cf19884fc3702dba929808172b2747db4c
Kurus00 <112712424+Kurus00@users.noreply.github.com>
2022-09-04 14:55:52 +0300

    Update Kurus hub - PLS DONATE.lua

--- Kurus hub - PLS DONATE.lua
+++ Kurus hub - PLS DONATE.lua
@@ -7,19 +7,19 @@ local Tab = Window:NewTab("Scripts")
 
 local Section = Tab:NewSection("PLS DONATE")
 
-Section:NewButton("AutoFarm", "Очень крутой скрипт", function()
+Section:NewButton("AutoFarm", "Autofarm", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/tzechco/roblox-scripts/main/PLS%20DONATE/autofarm.lua'))()
 end)
 
-Section:NewButton("Animations (Есть в хабе)", "Очень крутой скрипт", function()
+Section:NewButton("Animations (Avaible in hub)", "not visual", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/o5u3/emotesv2/main/script"))()
 end)
 
-Section:NewButton("V.G Hub (Есть в хабе)", "Очень крутой скрипт", function()
+Section:NewButton("V.G Hub (Avaible in Hub)", "Очень крутой скрипт", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
 end)
 
-Section:NewButton("AntiAFK (Может Кикнуть)", "Багганый не много", function()
+Section:NewButton("AntiAFK (Chance to kick)", "Багганый не много", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/LoadstringypVvhJBq4QNz", true))()
 end)
 
@@ -29,6 +29,6 @@ local Section = Tab:NewSection("kurusLK")
 
 local Section = Tab:NewSection("Kurus#6448")
 
-local Section = Tab:NewSection("Разраб русский!")
+local Section = Tab:NewSection("Developer russian!")
 
 
